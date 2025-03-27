26d38b50b7a64f877a44306647c344aafe5e97a0
HimeshPremji <himeshdmaheswary@gmail.com>
2025-03-27 23:11:10 +0500

    Change email field type from String to Object in User model

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
+  email: { type: Object, required: true, unique: true, trim: true },
   imageUrl: { type: String, required: true },
   cartItems: { type: Object, default: {} },
 }, { minimize: false });
